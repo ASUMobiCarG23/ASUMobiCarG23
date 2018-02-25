044d1cfc43498b41bb367865ac1fdd626faa6dd1
Mahmoud Refaat <36297937+m7oda97@users.noreply.github.com>
2018-02-25 21:43:27 +0200

    Add files via upload
    Version 2

---
+++ Mahmoud Refaat/sketch_feb25a.ino
@@ -0,0 +1,54 @@
+char t;
+
+void setup() {
+pinMode(13,OUTPUT);
+pinMode(12,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);
+
+Serial.begin(9600);
+
+}
+
+void loop() {
+if(Serial.available()){
+  t = Serial.read();
+  Serial.println(t);
+}
+
+if(t == '1'){
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,LOW);
+}
+
+else if(t == '2'){
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  digitalWrite(11,LOW);
+  digitalWrite(10,HIGH);
+}
+
+else if(t == '3'){
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,HIGH);
+  digitalWrite(10,LOW);
+}
+
+else if(t == '4'){
+  digitalWrite(13,HIGH);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+}
+ 
+else if(t == '5'){
+  digitalWrite(13,LOW);
+  digitalWrite(12,LOW);
+  digitalWrite(11,LOW);
+  digitalWrite(10,LOW);
+}
+delay(100);
+}
