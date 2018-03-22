cef36db0129ee9463037c9971868a60c469d4012
Mohamed3li3bdalazez <36374526+Mohamed3li3bdalazez@users.noreply.github.com>
2018-03-22 17:37:07 +0200

    Add files via upload

---
+++ interrupt exampel.ino
@@ -0,0 +1,26 @@
+//int led2 12;
+//int temp 10;
+void setup()
+{
+  pinMode(13, OUTPUT) ;
+  pinMode(12, OUTPUT) ;
+  pinMode(2, INPUT) ;
+  attachInterrupt(0, tempsens, FALLING);
+}
+void tempsens()
+{
+    if (digitalRead(2) == HIGH)
+        digitalWrite(12 , HIGH);
+    else 
+       digitalWrite(12 , LOW);     
+}
+
+void loop()
+{
+  interrupts();
+ digitalWrite(13, HIGH );   
+  delay(100);           
+ digitalWrite(13, LOW);   
+  delay(100);  
+
+}
