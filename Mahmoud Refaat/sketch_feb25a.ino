dc6e4a0aff0a07a9e069b23ae6732541f7ec4e02
Mahmoud Refaat <36297937+m7oda97@users.noreply.github.com>
2018-03-27 22:33:37 +0200

    Add files via upload

--- Mahmoud Refaat/sketch_feb25a.ino
+++ Mahmoud Refaat/sketch_feb25a.ino
@@ -1,6 +1,12 @@
 char t;
+#define ECHOPIN 8
+#define TRIGPIN 9
 
 void setup() {
+pinMode(ECHOPIN, INPUT);
+pinMode(TRIGPIN, OUTPUT);
+pinMode(2,OUTPUT);
+digitalWrite(2,HIGH);
 pinMode(13,OUTPUT);
 pinMode(12,OUTPUT);
 pinMode(11,OUTPUT);
@@ -10,45 +16,68 @@ Serial.begin(9600);
 
 }
 
-void loop() {
+void loop() {  
+digitalWrite(2,HIGH);
 if(Serial.available()){
   t = Serial.read();
   Serial.println(t);
 }
 
-if(t == '1'){
+digitalWrite(TRIGPIN, LOW);
+delayMicroseconds(2);
+digitalWrite(TRIGPIN, HIGH);
+delayMicroseconds(10);
+digitalWrite(TRIGPIN, LOW);
+
+float distance = pulseIn(ECHOPIN, HIGH);
+distance = distance/58;
+
+Serial.print(distance);
+Serial.println(" cm");
+delay(50);
+
+if(distance <= 50) {
+digitalWrite(13,LOW);
+digitalWrite(12,LOW);
+digitalWrite(11,LOW);
+digitalWrite(10,LOW);  
+}
+else {
+if(t == 'F'){
   digitalWrite(13,HIGH);
   digitalWrite(12,LOW);
   digitalWrite(11,HIGH);
   digitalWrite(10,LOW);
 }
 
-else if(t == '2'){
+else if(t == 'B'){
   digitalWrite(13,LOW);
   digitalWrite(12,HIGH);
   digitalWrite(11,LOW);
   digitalWrite(10,HIGH);
 }
 
-else if(t == '3'){
+else if(t == 'R'){
   digitalWrite(13,LOW);
   digitalWrite(12,LOW);
   digitalWrite(11,HIGH);
   digitalWrite(10,LOW);
 }
 
-else if(t == '4'){
+else if(t == 'L'){
   digitalWrite(13,HIGH);
   digitalWrite(12,LOW);
   digitalWrite(11,LOW);
   digitalWrite(10,LOW);
 }
  
-else if(t == '5'){
+else if(t == 'S'){
   digitalWrite(13,LOW);
   digitalWrite(12,LOW);
   digitalWrite(11,LOW);
   digitalWrite(10,LOW);
 }
+}
 delay(100);
+
 }
