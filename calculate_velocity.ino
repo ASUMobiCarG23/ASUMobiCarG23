5aaff63bc96a1848a300036c73479a58e19db66f
ahmad-ra <36510453+ahmad-ra@users.noreply.github.com>
2018-07-03 18:02:28 +0200

    Add files via upload

---
+++ calculate_velocity.ino
@@ -0,0 +1,16 @@
+float Calcvel (){
+unsigned long start  ,endd;
+photo =0 ; 
+Choosetryy();
+while (photo<42){
+  tryy();
+  if (photo==1)  { start= millis(); }
+ 
+  if (photo==42)  { endd= millis(); }
+
+  }
+ 
+   float vel = (((float)(endd-start))/1000.0);
+
+return((circumference)/vel);
+  }
