1ec0906dabfe7e4c023a315b94ba48e23f75c00e
ahmad-ra <36510453+ahmad-ra@users.noreply.github.com>
2018-07-06 00:32:10 +0200

    Add files via upload

---
+++ counting_pulses_of_photo_interruptor_without_using_interrupts.ino
@@ -0,0 +1,11 @@
+///////////////those two fns for counting pulses in photointerruptor ////////
+bool flag;
+void Choosetryy(){ if(digitalRead(2)) flag=LOW ; else flag=HIGH ;}
+
+void tryy (){
+ 
+if (digitalRead(2) && flag )      {photo++ ;flag = LOW ;}
+if (!digitalRead(2)&& !flag)      {photo++ ;flag =HIGH ;}
+
+}
+
